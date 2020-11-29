6bc1f7fd2a8b578b20949b15378600203764a349
MaksGrishaev <gm.grishaev@gmail.com>
2020-11-29 19:10:11 +0200

    added parse_cookie function

--- main.py
+++ main.py
@@ -33,19 +33,40 @@ if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=') == {'name': 'ferret'}
 
 
-# def parse_cookie(query: str) -> dict:
-#     if len(query) == 0 or query.count(';') == 0 or query.count('=') == 0:
-#         return {}
-#     result_dict = {}
-#     data_list = query.split(';')
-#     for dict_item in data_list:
-#         if len(dict_item) != 0 and dict_item.count('=') > 0:
-#             result_dict[dict_item.split('=')[0]] = dict_item[dict_item.find('=') + 1::]
-#     return result_dict
-#
-#
-# if __name__ == '__main__':
-#     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-#     assert parse_cookie('') == {}
-#     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-#     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+def check_str_without_spaces(query: str) -> bool:
+    for ch in query:
+        if ch.isspace():
+            return False
+    return True
+
+def parse_cookie(query: str) -> dict:
+    if len(query) == 0 or query.count(';') == 0 or query.count('=') == 0:
+        return {}
+    result_dict = {}
+    data_list = query.split(';')
+    for dict_item in data_list:
+        if len(dict_item) != 0 and dict_item.count('=') > 0:
+            key = dict_item.split('=')[0]
+            value = dict_item.split('=')[1]
+            if len(key) > 0 and len(value) > 0 :
+                if check_str_without_spaces(key) and check_str_without_spaces(value):
+                    result_dict[dict_item.split('=')[0]] = dict_item[dict_item.find('=') + 1::]
+    return result_dict
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+
+    assert parse_cookie('name=Dima;age') == {'name': 'Dima'}
+    assert parse_cookie('first name=Dima;') == {}
+    assert parse_cookie('name=Dima;age=') == {'name': 'Dima'}
+    assert parse_cookie('nameDima;age=25') == {'age': '25'}
+    assert parse_cookie('=Dima;age=25') == {'age': '25'}
+    assert parse_cookie('name=Dima;age=25;height=175;') == {'name': 'Dima', 'age': '25', 'height': '175'}
+    assert parse_cookie(';') == {}
+    assert parse_cookie(';;') == {}
+    assert parse_cookie(' =  ;   =  ') == {}
+    assert parse_cookie('   ;     ') == {}
